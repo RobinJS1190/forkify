4bf1e8aedb666c99eb3d35d8f43a6b8b5e362282
RobinJS1190 <robin.koeck@gmx.at>
2022-11-19 17:12:31 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '49f268c9-2d21-4b78-8c75-d7dc070d5902';
+export const MODAL_CLOSE_SEC = 2.5;
